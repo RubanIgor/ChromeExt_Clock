1220ffd9ee24e6d35ddcbed7f0e75bbf2c4f50a3
Ruban Igor <olapmail@ukr.net>
2016-06-06 00:05:37 +0300

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,24 @@
+﻿function completeZero(param) {
+	return ("0" + param).slice(-2);
+}
+
+Clock = function() {
+	
+	this.get_time = function() {
+		var now = new Date();
+		return completeZero(now.getHours()) + ":" + completeZero(now.getMinutes()) + ":" + completeZero(now.getSeconds());
+	}, 
+	
+	this.draw = function() {		
+		document.getElementById("clock").innerText = this.get_time();
+	}
+}
+
+window.onload = function() {
+	var clock = new Clock();
+	window.setInterval(
+		function() {
+			clock.draw();
+		}, 
+		200);
+}		
